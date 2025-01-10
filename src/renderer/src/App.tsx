b20371b7d6205f03f1e5ef9b8698b135e24e729b
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-01-10 12:39:21 +0900

    Preparing a container for the ui form of this crud operation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,9 @@
 const App: React.FC = () => {
   return (
-    <div><button className="btn btn-primary">Hello world</button></div>
+    <div className="container d-flex flex-col align-items-center vh-100">
+nde      <button className="btn btn-primary">sample button</button>
+    </div>
+
   )
 }
 export default App
