ff5de989f2dd7d82302e2675bf82acdb9e70e181
Sherwin <94516340+Smontan@users.noreply.github.com>
2025-01-10 12:37:41 +0900

    Define the interface Window.api

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -3,6 +3,29 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    api: {
+      createUser: (firstname: string, lastname: string, birthdate: Date, email: string) => void
+      readUser: () => void
+      updateUser: (
+        id: number,
+        firstname: string,
+        lastname: string,
+        birthdate: Date,
+        email: string
+      ) => void
+      deleteUser: (id: number) => void
+      onCreateUserResponse: (
+        callback: (event: any, response: { success: boolean; err?: string }) => void
+      ) => void
+      onReadUsersResponse: (
+        callback: (event: any, response: { success: boolean; user?: any[]; err?: string }) => void
+      ) => void
+      onUpdateUserResponse: (
+        callback: (event: any, response: { success: boolean; err?: string }) => void
+      ) => void
+      onDeleteUserResponse: (
+        callback: (event: any, response: { success: boolean; err?: string }) => void
+      ) => void
+    }
   }
 }
